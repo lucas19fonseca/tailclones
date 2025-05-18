ffd4bf8025e8dc7a4fd8cae43ab3f928e32f3806
lucas19fonseca <lucas19fonseca@gmail.com>
2025-05-18 20:01:56 -0300

    uptade/home-overflow

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -4,29 +4,31 @@ import Amazon from "../../assets/amazon_logo.png"
 
 export default function Home() {
     return (
-        <div className="home-container h-screen bg-black">
-            <div className="grid grid-cols-1 md:grid-cols-3 h-full text-white">
-                {/* Spotify */}
-                <div className="bg-main-green flex justify-center items-center transition-transform hover:scale-125  hover:brightness-110  duration-500">
-                    <a href="/spotify" className="flex justify-center items-center">
-                        <img src={Spotify} alt="Logo Spotify" className="h-20 drop-shadow-lg" />
-                    </a>
-                </div>
+        <body>
+            <div className="home-container h-screen bg-black">
+                <div className="grid grid-cols-1 md:grid-cols-3 h-full text-white overflow-hidden">
+                    {/* Spotify */}
+                    <div className="bg-main-green flex justify-center items-center transition-transform hover:scale-125  hover:brightness-110  duration-500">
+                        <a href="/spotify" className="flex justify-center items-center">
+                            <img src={Spotify} alt="Logo Spotify" className="h-20 drop-shadow-lg" />
+                        </a>
+                    </div>
 
-                {/* Facebook */}
-                <div className="bg-blue-900 flex justify-center items-center transition-transform hover:scale-125 hover:brightness-110 duration-500">
-                    <a href="/facebook" className="flex justify-center items-center">
-                        <img src={Facebook} alt="Logo Facebook" className="h-20 drop-shadow-lg" />
-                    </a>
-                </div>
+                    {/* Facebook */}
+                    <div className="bg-blue-900 flex justify-center items-center transition-transform hover:scale-125 hover:brightness-110 duration-500">
+                        <a href="/facebook" className="flex justify-center items-center">
+                            <img src={Facebook} alt="Logo Facebook" className="h-20 drop-shadow-lg" />
+                        </a>
+                    </div>
 
-                {/* Amazon */}
-                <div className="bg-[#232F3E] flex justify-center items-center transition-transform hover:scale-125 hover:brightness-110  duration-500">
-                    <a href="/amazon" className="flex justify-center items-center">
-                        <img src={Amazon} alt="Logo Amazon" className="h-20 drop-shadow-lg" />
-                    </a>
+                    {/* Amazon */}
+                    <div className="bg-[#232F3E] flex justify-center items-center transition-transform hover:scale-125 hover:brightness-110  duration-500">
+                        <a href="/amazon" className="flex justify-center items-center">
+                            <img src={Amazon} alt="Logo Amazon" className="h-20 drop-shadow-lg" />
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </body>
     )
 }
